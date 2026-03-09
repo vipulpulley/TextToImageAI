859cf05572a7c7c7d1c27253164b34d267e268f3
vipulpulley <this.is.vipulsingh@gmail.com>
2026-03-09 20:15:51 +0530

    Intial working code

--- webui-user.bat
+++ webui-user.bat
@@ -1,9 +1,10 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Program Files\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set NO_PROXY=localhost,127.0.0.1,::1
+set COMMANDLINE_ARGS=--xformers --skip-python-version-check --theme dark --autolaunch --skip-install --always-offload-from-vram --listen
 
 @REM Uncomment following code to reference an existing A1111 checkout.
 @REM set A1111_HOME=Your A1111 checkout dir
